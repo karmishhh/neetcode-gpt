91e940941d2167d219f6ddf8931217563cf84df3
Kartik Mishra <55404534+karmishhh@users.noreply.github.com>
2026-09-03 14:02:47 +0530

    Update Single neuron forward pass (Build a Neural Net 1/5)

--- foundations/neuron.py
+++ foundations/neuron.py
@@ -11,8 +11,8 @@ class Solution:
         return np.maximum(0, x)
 
     def forward(self, x: NDArray[np.float64], w: NDArray[np.float64], b: float, activation: str) -> float:
-        out = np.matmul(x, w) + b
+        z = np.matmul(x, w) + b
         if activation == 'relu':
-            return np.round(self.relu(out),5)
+            return np.round(self.relu(z),5)
         elif activation == 'sigmoid':
-            return np.round(self.sigmoid(out),5) 
+            return np.round(self.sigmoid(z),5) 
