3c9c400521d8b44c5c7aff1cc2ecedbecabc6141
Kartik Mishra <55404534+karmishhh@users.noreply.github.com>
2026-09-03 14:02:09 +0530

    Implement Single neuron forward pass (Build a Neural Net 1/5)

---
+++ foundations/neuron.py
@@ -0,0 +1,18 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+    
+    def relu(self, x):
+        return np.maximum(0, x)
+
+    def forward(self, x: NDArray[np.float64], w: NDArray[np.float64], b: float, activation: str) -> float:
+        out = np.matmul(x, w) + b
+        if activation == 'relu':
+            return np.round(self.relu(out),5)
+        elif activation == 'sigmoid':
+            return np.round(self.sigmoid(out),5) 
